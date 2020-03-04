80a2dbf262032450af854ebc0ce252cac22f4110
Fernando Jesus <fernandoj.5a@stelo.com.br>
2020-03-04 16:35:59 -0300

    Update

---
+++ learning-props/src/app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+import React from 'react'
+import Title from './title'
+
+const App = React.createClass({
+  render: () => {
+    return (
+      <div>
+        <Title name='Fernando Jesus' />
+      </div>
+    )
+  }
+})
+
+export default App
